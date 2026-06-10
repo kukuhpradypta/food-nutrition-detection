b4765b76855a150464975b4017d88b2239986aaf
kukuh <kukuhpradypta@gmail.com>
2026-06-10 09:00:47 +0700

    add suport postgres

--- api/config.py
+++ api/config.py
@@ -10,7 +10,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Database
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:password@localhost:3366/vitality")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Model
 CHECKPOINT_PATH = os.path.join(BASE_DIR, "checkpoints", "best_model.pth")
